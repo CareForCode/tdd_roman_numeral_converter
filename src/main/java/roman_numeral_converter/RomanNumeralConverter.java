e6c50fdcb3292d76da1a585f64a81b2fdbd5dca8
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 09:02:44 +0100

    convert 1

--- src/main/java/roman_numeral_converter/RomanNumeralConverter.java
+++ src/main/java/roman_numeral_converter/RomanNumeralConverter.java
@@ -2,13 +2,16 @@ package roman_numeral_converter;
 
 public class RomanNumeralConverter {
 
+    public static final String ROMAN_5 = "V";
+    public static final String ROMAN_1 = "I";
+
     public String convert(int value) {
         StringBuilder result = new StringBuilder();
         if (value == 5) {
-            return RomanNumbers.FIVE.getValue();
+            return ROMAN_5;
         }
         for (int i = 0; i < value; i++) {
-            result.append(RomanNumbers.ONE.getValue());
+            result.append(ROMAN_1);
         }
         return result.toString();
     }
