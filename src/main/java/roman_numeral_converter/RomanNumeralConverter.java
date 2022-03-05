761553d45f4427c4f1112ddeeea397bd52cd2250
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 08:33:40 +0100

    convert 1

--- src/main/java/roman_numeral_converter/RomanNumeralConverter.java
+++ src/main/java/roman_numeral_converter/RomanNumeralConverter.java
@@ -1,4 +1,7 @@
 package roman_numeral_converter;
 
 public class RomanNumeralConverter {
+    public String convert(int value) {
+        return "I";
+    }
 }
--- src/test/java/roman_numeral_converter/RomanNumeralConverterTest.java
+++ src/test/java/roman_numeral_converter/RomanNumeralConverterTest.java
@@ -2,9 +2,18 @@ package roman_numeral_converter;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RomanNumeralConverterTest {
     @Test
     void initConverter() {
         RomanNumeralConverter converter = new RomanNumeralConverter();
+    }
+
+    @Test
+    void todo() {
+        RomanNumeralConverter converter = new RomanNumeralConverter();
+
+        assertThat(converter.convert(1)).isEqualTo("I");
     }
 }
