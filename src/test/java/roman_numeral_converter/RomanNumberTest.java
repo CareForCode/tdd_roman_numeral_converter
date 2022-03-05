207c14cbf4bb711125402fc7639b3f6678d99a6d
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 10:33:07 +0100

    convert 1

--- src/test/java/roman_numeral_converter/RomanNumberTest.java
+++ src/test/java/roman_numeral_converter/RomanNumberTest.java
@@ -1,13 +1,16 @@
 package roman_numeral_converter;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanNumberTest {
 
-    @Test
-    void ofNumber_1_isI() {
-        assertThat(RomanNumber.ofNumber(1)).isEqualByComparingTo(RomanNumber.I);
+    @ParameterizedTest
+    @CsvSource({"1,I"})
+    void ofNumber_1_isI(int arabicNumber, char expectedRomanNumber) {
+        assertThat(RomanNumber.ofNumber(1)).isEqualByComparingTo(RomanNumber.ofChar(expectedRomanNumber));
     }
 }
