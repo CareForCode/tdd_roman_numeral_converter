262881c1e30b0712db4af71b7b38acb910faceab
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 09:07:46 +0100

    convert 1

--- src/main/java/roman_numeral_converter/RomanNumeralConverter.java
+++ src/main/java/roman_numeral_converter/RomanNumeralConverter.java
@@ -6,14 +6,6 @@ public class RomanNumeralConverter {
     public static final String I = "I";
 
     public String convert(int value) {
-        if (value < 4) {
-            return I.repeat(Math.max(0, value));
-        } else if (value == 4) {
-            return I + V;
-        } else if (value == 6) {
-            return V + I;
-        } else {
-            return V;
-        }
+        return I.repeat(Math.max(0, value));
     }
 }
--- src/test/java/roman_numeral_converter/RomanNumeralConverterTest.java
+++ src/test/java/roman_numeral_converter/RomanNumeralConverterTest.java
@@ -10,7 +10,7 @@ public class RomanNumeralConverterTest {
     private final RomanNumeralConverter converter = new RomanNumeralConverter();
 
     @ParameterizedTest
-    @CsvSource({"1,I", "2,II", "3,III", "5,V", "4, IV", "6,VI"})
+    @CsvSource({"1,I", "2,II", "3,III", "4, IIII"})
     void convert1_getI(int number, String expectedRomanNumber) {
         assertThat(converter.convert(number)).isEqualTo(expectedRomanNumber);
     }
