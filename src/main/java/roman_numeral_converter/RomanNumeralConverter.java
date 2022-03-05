a1f3b10f778aa9e5836af04163ff969f5743e3a9
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 09:22:31 +0100

    convert 1

--- src/main/java/roman_numeral_converter/RomanNumeralConverter.java
+++ src/main/java/roman_numeral_converter/RomanNumeralConverter.java
@@ -11,19 +11,17 @@ public class RomanNumeralConverter {
         int remainder = value;
         if (value == 10) {
             return X;
-        }
-        if (value >= 5) {
-            result =  V;
-            remainder -= 5;
-        }
-        if (remainder == 4) {
-            if (value == 4) {
-                return I + V;
-            } else {
-                return I + X;
+        } else if (value == 4) {
+            return I + V;
+        } else if (value == 9) {
+            return I + X;
+        } else {
+            if (value >= 5) {
+                result =  V;
+                remainder -= 5;
             }
+            result += I.repeat(Math.max(0, remainder));
         }
-        result += I.repeat(Math.max(0, remainder));
         return result;
     }
 }
