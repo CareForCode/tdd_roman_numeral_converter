3359b44f74b61e3549d2154270d3b2a467a48cb3
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 08:36:28 +0100

    convert 1

--- src/test/java/roman_numeral_converter/RomanNumeralConverterTest.java
+++ src/test/java/roman_numeral_converter/RomanNumeralConverterTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanNumeralConverterTest {
 
+    private final RomanNumeralConverter converter = new RomanNumeralConverter();
+
     @Test
     void initConverter() {
         RomanNumeralConverter converter = new RomanNumeralConverter();
@@ -13,15 +15,11 @@ public class RomanNumeralConverterTest {
 
     @Test
     void convert1_getI() {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
-
         assertThat(converter.convert(1)).isEqualTo(RomanNumeralConverter.ROMAN_1);
     }
 
     @Test
     void convert2_getII() {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
-
         assertThat(converter.convert(2)).isEqualTo("II");
     }
 }
