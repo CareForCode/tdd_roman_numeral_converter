bcc23ea284965bd161192fcc38538fcc0ba77ce1
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 09:11:48 +0100

    convert 1

--- src/test/java/roman_numeral_converter/RomanNumeralConverterTest.java
+++ src/test/java/roman_numeral_converter/RomanNumeralConverterTest.java
@@ -10,7 +10,7 @@ public class RomanNumeralConverterTest {
     private final RomanNumeralConverter converter = new RomanNumeralConverter();
 
     @ParameterizedTest
-    @CsvSource({"1,I", "2,II", "3,III", "4, IIII", "5,V", "6,VI"})
+    @CsvSource({"1,I", "2,II", "3,III", "4, IIII", "5,V", "6,VI", "7,VII", "8,VIII", "9,VIIII"})
     void convert1_getI(int number, String expectedRomanNumber) {
         assertThat(converter.convert(number)).isEqualTo(expectedRomanNumber);
     }
