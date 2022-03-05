b4c5a69806585d3d8d2cb0e86d5ed9dbc204cd66
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 09:08:23 +0100

    convert 1

--- src/main/java/roman_numeral_converter/RomanNumeralConverter.java
+++ src/main/java/roman_numeral_converter/RomanNumeralConverter.java
@@ -6,6 +6,9 @@ public class RomanNumeralConverter {
     public static final String I = "I";
 
     public String convert(int value) {
+        if (value == 5) {
+            return V;
+        }
         return I.repeat(Math.max(0, value));
     }
 }
