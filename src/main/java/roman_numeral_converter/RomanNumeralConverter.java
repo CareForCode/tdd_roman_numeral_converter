f2e97feaa9f5456941ee187414ad6733fe6c9827
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 10:11:26 +0100

    convert 1

--- src/main/java/roman_numeral_converter/RomanNumeralConverter.java
+++ src/main/java/roman_numeral_converter/RomanNumeralConverter.java
@@ -5,11 +5,11 @@ public class RomanNumeralConverter {
     public String convert(int value) {
         StringBuilder result = new StringBuilder();
         if (value >= 10) {
-            return getRepeatedNumerals(value/10, RomanNumbers.X);
+            result.append(getRepeatedNumerals(value/10, RomanNumbers.X));
         } else if (value == 4) {
-            return RomanNumbers.I.getValue() + RomanNumbers.V.getValue();
+            result.append(RomanNumbers.I.getValue() + RomanNumbers.V.getValue());
         } else if (value == 9) {
-            return RomanNumbers.I.getValue() + RomanNumbers.X.getValue();
+            result.append(RomanNumbers.I.getValue() + RomanNumbers.X.getValue());
         } else {
             int remainder = value;
             if (value >= 5) {
