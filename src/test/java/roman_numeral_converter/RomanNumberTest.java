80d2681365a717fa9b1198d91ddae171e1929388
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-05 10:31:32 +0100

    convert 1

---
+++ src/test/java/roman_numeral_converter/RomanNumberTest.java
@@ -0,0 +1,13 @@
+package roman_numeral_converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RomanNumberTest {
+
+    @Test
+    void todo() {
+        assertThat(RomanNumber.ofNumber(1)).isEqualByComparingTo(RomanNumber.I);
+    }
+}
